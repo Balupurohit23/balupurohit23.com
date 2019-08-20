a2478af54faa6f1ef4620fb4579552c201392215
Balu Purohit <balupurohit23@gmail.com>
2019-08-21 03:09:10 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { Link } from "gatsby"
 import DefaultButton from '../components/button'
 import { SocialIcon } from 'react-social-icons'
 
