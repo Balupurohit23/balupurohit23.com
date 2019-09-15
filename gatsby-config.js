a8acea5d5e05455b01eb8607b24230fe6aaf9160
m1047237 <m1047237@mindtree.com>
2019-09-16 00:51:10 +0530

    SEO changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,9 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-const path = require('path')
-const siteUrl = process.env.URL || process.env.DEPLOY_URL || `https://www.balupurohit23.com`
+const path = require("path")
+const siteUrl =
+  process.env.URL || process.env.DEPLOY_URL || `https://www.balupurohit23.com`
 
 module.exports = {
   /* MetaData of my site */
@@ -21,25 +22,25 @@ module.exports = {
 
   /* Gatsby plugins */
   plugins: [
-  	{
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `BaluPurohit23`,
         short_name: `balupurohit23`,
         description: `Personal website of Balu Purohit.`,
-        lang: 'en',
-		  icons: [
-		    {
-		      src: `/icons/android-chrome-192x192.png`,
-		      sizes: `192x192`,
-		      type: `image/png`,
-		    },
-		    {
-		      src: `/icons/android-chrome-512x512.png`,
-		      sizes: `512x512`,
-		      type: `image/png`,
-		    },
-		  ], // Add or remove icon sizes as desired
+        lang: "en",
+        icons: [
+          {
+            src: `/icons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/icons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ], // Add or remove icon sizes as desired
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
@@ -48,13 +49,11 @@ module.exports = {
     },
 
     `gatsby-plugin-react-helmet`,
-    'gatsby-plugin-sass',
+    "gatsby-plugin-sass",
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
-    'gatsby-plugin-robots-txt',
     `gatsby-plugin-catch-links`,
-
 
     {
       resolve: `gatsby-source-filesystem`,
@@ -73,30 +72,140 @@ module.exports = {
     },
 
     {
-      resolve: 'gatsby-plugin-root-import',
+      resolve: "gatsby-plugin-root-import",
       options: {
-        src: path.join(__dirname, 'src'),
-        pages: path.join(__dirname, 'src/pages'),
-        components: path.join(__dirname, 'src/components'),
-        layout: path.join(__dirname, 'src/layout'),
-        style: path.join(__dirname, 'src/style'),
-        utils: path.join(__dirname, 'src/utils')
-      }
+        src: path.join(__dirname, "src"),
+        pages: path.join(__dirname, "src/pages"),
+        components: path.join(__dirname, "src/components"),
+        layout: path.join(__dirname, "src/layout"),
+        style: path.join(__dirname, "src/style"),
+        utils: path.join(__dirname, "src/utils"),
+      },
     },
 
     {
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      plugins: [],
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [],
       },
     },
 
     {
       resolve: `gatsby-plugin-disqus`,
       options: {
-        shortname: `balupurohit23`
+        shortname: `balupurohit23`,
+      },
+    },
+
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', allow: '/' }]
       }
     },
-  ],
 
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        exclude: [`/admin`, `/tags/links`]
+      }
+    },
+
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                author
+                siteUrl
+                siteVerification {
+                  google
+                  bing
+                }
+                social {
+                  twitter
+                }
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        date(formatString: "MMMM DD, YYYY")
+                        path
+                        title
+                        description
+                        tags
+                        author  
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "BaluPurohit23 | RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/blog/",
+          },
+        ],
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-148025931-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "balupurohit23.com",
+      },
+    },
+  ],
 }
